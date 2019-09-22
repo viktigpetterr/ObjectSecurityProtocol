117b8bb3591ecec92d5f2b74080753029460e656
diocles1 <viktor.grasljunga@gmail.com>
2019-09-22 18:38:51 +0200

    Addded inital logic for client and server classes.

--- Client.py
+++ Client.py
@@ -1,9 +1,22 @@
 import socket
 
-def class Client():
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
 
-    def listener():
+class Client:
+    #Constructor
+    def __init__(localAdress, port):
+        self.localAdress = localAdress
+        self.port = port
+    def run(self):
+        print("Welcome to server side!")
+        print("Instance is running on: " + str(self.localAdress) + ":" + str(self.port))
+        while(True):
+            pass
+    def listener(self):
+        print("You called on the listener")
+    def sender(self):
+        print("You called on the sender")
 
-    def sender():
-
-new Client()
+client = Client(UDP_IP, UDP_PORT )
+client.run()
--- Server.py
+++ Server.py
@@ -1,4 +1,17 @@
 import socket
 
-def class Server():
-    
+class Server:
+    #Constructor
+    def __init__(localAdress, port):
+        self.localAdress = localAdress
+        self.port = port
+
+    def run():
+        print("Welcome to client side!")
+        while(True)
+        
+    def listener():
+
+    def sender():
+
+new Server()
