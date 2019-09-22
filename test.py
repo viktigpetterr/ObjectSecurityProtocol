56e888be7a911154bead08d051213264b47408e5
diocles1 <viktor.grasljunga@gmail.com>
2019-09-22 20:44:38 +0200

    Added py file for testing the logic.

---
+++ test.py
@@ -0,0 +1,8 @@
+# Script for testing the logic for the protocol. 
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
