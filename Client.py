b666d2e708088e8823a03ddb1b921c389b397f1e
diocles1 <viktor.grasljunga@gmail.com>
2019-09-22 19:30:58 +0200

    Added newlines

--- Client.py
+++ Client.py
@@ -8,13 +8,16 @@ class Client:
     def __init__(self, localAdress, port):
         self.localAdress = localAdress
         self.port = port
+
     def run(self):
         print("Welcome to client side!")
         print("Instance is running on: " + str(self.localAdress) + ":" + str(self.port))
         while(True):
             pass
+
     def listener(self):
         print("You called on the listener")
+        
     def sender(self):
         print("You called on the sender")
 
--- Server.py
+++ Server.py
@@ -8,13 +8,16 @@ class Server:
     def __init__(self, localAdress, port):
         self.localAdress = localAdress
         self.port = port
+
     def run():
         print("Welcome to client side!")
         print("Instance is running on: " + str(self.localAdress) + ":" + str(self.port))
         while(True):
             pass
+
     def listener(self):
         print("You called on the listener")
+        
     def sender(self):
         print("You called on the sender")
 
