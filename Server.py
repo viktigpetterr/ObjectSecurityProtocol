e7ed5497cf12a59588b0fd8f5ec98febad3e01cd
Carltern <carl.ternby@debricked.com>
2019-09-23 10:18:06 +0200

    Added simple server-side logics

--- Server.py
+++ Server.py
@@ -2,24 +2,34 @@ import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+MESSAGE = "Hello World!"
 
 class Server:
     #Constructor, we need to add self variable in constructor and methods in order to point to the object. 
     def __init__(self, localAdress, port):
         self.localAdress = localAdress
         self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
 
-    def run():
-        print("Welcome to client side!")
+    def run(self):
+        print("Welcome to server side!")
         print("Instance is running on: " + str(self.localAdress) + ":" + str(self.port))
         while(True):
-            pass
+            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            if data is not None:
+                 print "received message:", data
+
 
     def listener(self):
         print("You called on the listener")
+        
+       
 
     def sender(self):
         print("You called on the sender")
+        self.sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
         
 if __name__ == "__main__":    
     server = Server(UDP_IP, UDP_PORT)
